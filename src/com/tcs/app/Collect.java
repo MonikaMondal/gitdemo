ac4b8d24413f002bd448e686e0772e24270a153e
Chotu <Chotu@192.168.0.5>
2017-12-13 21:50:03 +0530

    commit

---
+++ src/com/tcs/app/Collect.java
@@ -0,0 +1,7 @@
+package com.tcs.app;
+
+public class Collect {
+public static void main(String[] args) {
+	System.out.println("A collection class");
+}
+}
--- src/com/tcs/app/While.java
+++ src/com/tcs/app/While.java
@@ -3,7 +3,7 @@ package com.tcs.app;
 public class While {
 	public static void main(String args[]){
 		int i=1;
-		while(i<10){
+		while(i<20){
 			System.out.println(i);
 			i++;
 		}
